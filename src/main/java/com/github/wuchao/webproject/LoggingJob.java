ca53616ca82a5e16cc501ad1e12e4c52b7012071
wuchao <chao@gmx.com>
2019-03-13 15:16:26 +0800

    initial commit.

--- src/main/java/com/github/wuchao/webproject/LoggingJob.java
+++ src/main/java/com/github/wuchao/webproject/LoggingJob.java
@@ -2,10 +2,12 @@ package com.github.wuchao.webproject;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Profile(value = {"dev"})
 @Component
 @Slf4j
 public class LoggingJob implements CommandLineRunner {
